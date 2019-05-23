df3939f4332d9b56662fce6dd069f966c87440dd
Daniel Shih <dzshih@uci.edu>
2019-05-22 23:53:37 -0700

    Updated agent to learn to show wheat, reset each mission to not have wheat deselected by default, reward adjustments

--- src/farmworld.py
+++ src/farmworld.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import math
+import time
 
 GATE_COORDINATES = (0, 0)
 
@@ -13,17 +14,18 @@ class World:
         self.coords = (0, 0)
         self.state = (0, 0)
         self.total_reward = 0
-        self.total_steps = 50
+        self.total_steps = 100
         self.sheeps = set()
 
-        self.actions = 4
+        self.actions = 6
         self.world = np.zeros((16, 16))
         self.world_state = None
         self.shouldReturn = False
+        self.holding_wheat = False
         self.stall = False
 
     def getValidActions(self):
-        return [0, 1, 2, 3]
+        return [0, 1, 2, 3, 4, 5]
 
     def game_status(self):
         if self.total_steps > 0:
@@ -44,11 +46,7 @@ class World:
         x, z = self.state
         deltax = x - 0.5
         deltaz = z - 0.5
-        if self.stall:
-            self.stall = False
-            return 4
-
-        self.stall = True
+        time.sleep(0.25)
         if (x, z) == (0, 0):
             self.shouldReturn = False
             # WE SHOULD TELEPORT THE AGENT OR SOMETHING HERE????
@@ -84,12 +82,25 @@ class World:
                     row, col = self.state
                     dist = (x-row)**2 + (z-col)**2
                     if dist <= 4:
+                        # Near sheep, show wheat. (hard-coded policy when close, but agent could also learn to show it when approaching)
+                        agent_host.sendCommand("hotbar.2 1")
+                        agent_host.sendCommand("hotbar.2 0")
+
                         if (row, col) == (0, 0):
                             reward += 300
                         elif i["id"] not in self.sheeps:
                             self.sheeps.add(i["id"])
                             reward += 100
                             self.shouldReturn = True
+                        if action == 4: # Good if the action shows wheat near a sheep:
+                            reward += 100
+                        elif action == 5:
+                            reward -= 100
+               
+                    else:
+                        if action in (4,5): # Punish agent for wasting show/hide wheat action when not near sheep
+                            reward -= 100
+
                     self.world[x][z] = 2
 
                     # Less negative reward when agent is closer to sheep
